bacaf8857a043887aca7de735042306ff160c630
ShadHoe26 <130573192+ShadHoe26@users.noreply.github.com>
2025-08-13 19:11:55 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask, request, render_template
+import numpy as np
+import pandas as pd
+import pickle
+import os
+
+model_path = os.path.join(os.path.dirname(__file__), 'model.pkl')
+with open(model_path, 'rb') as f:
+    model = pickle.load(f)
+
+
+#flask app
+app=Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+@app.route("/predict",methods=['POST'])
+def predict():
+    features=request.form['feature']
+    features_list=features.split(',')
+    np_features=np.asarray(features_list,dtype=np.float32)
+    pred=model.predict(np_features.reshape(1,-1))
+
+    output=["cancerous" if pred[0]==1 else "not cancerous"]
+    return render_template('index.html',message=output)
+
+#python main
+if __name__ == "__main__":
+    app.run(debug=True)
